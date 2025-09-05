1c243d7dafc9cf734b6a74c476c27a13eb27a187
Alok Rai <101451098+AlokRai123@users.noreply.github.com>
2025-09-06 01:28:50 +0530

    make a validator

---
+++ validators/chat.validators.ts
@@ -0,0 +1,16 @@
+import z from 'zod';
+
+const chatSchema = z.object({
+    userQuery : z.string().min(2).max(10000),
+    userSessionId : z.string({message : "x-user-session header is required"})
+    .uuid("Invalid session id"),
+});
+
+//type
+type ChatData = z.infer<typeof chatSchema>;
+
+const validateChat = (data : ChatData) =>{
+    return chatSchema.safeParse(data);
+};
+
+export {validateChat};
